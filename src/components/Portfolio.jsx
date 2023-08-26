0e57b281247a7bc9e1261325e98e52df173fc5b9
zisam shaikh <zisamshaikh7@gmail.com>
2023-08-27 00:38:23 +0600

    commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -13,42 +13,42 @@ const Portfolo = () => {
               <li>
                 <div className="cardtab">
                   <a href="https://marspurifier.com/" >
-                    <img src="img/portfolio/1.1.jpg" alt="" />
+                    <img src="https://previews.dropbox.com/p/thumb/ACCdu00msI4kDUmdc5gFjUO9F7QV8IAGeSOX0aw6GBKf5m2rVR78HXK-tmsEkBPGCooQ9H2z7E3-lH_xjGu38868WmR4oDIrM57uSeQFEB2AlzRnF6MzsnDua-rngq3L8_fSDM0DhKGtm4n55vuYRDovwn6E7j0m3GSlK5g5mt3hFnjvaZvE3sOwlswXAeWCCXqad-49bAKyCNBAfyRQk6siiJrmxcQyytXmBYJVF_DPr8WKMuyaFOLMqRJaUGtQca_yOzRY5Y4FlN7WYOTbBK31LCNOOJTeRrI0E2Hx0GXkDkbe1FseCPysr4Khign3eHvHuSrPGRKzvykU5K0zYhND/p.jpeg" alt="" />
                   </a>
                 </div>
               </li>
               <li>
                 <div className="cardtab">
                   <a  >
-                    <img src="img/portfolio/5.1.jpg" alt="" />
+                    <img src="https://previews.dropbox.com/p/thumb/ACBObGNAv8zRzeUARXWw6eoh6vwXzBnPC-srVB5RLy12jQegzNcTO_weWXxxBtHrs0je2ASPb7JVzXx5y4FpHB8wufokzMUmSKYnrmW_m1dDRwLumgNS2mSEC9knkQntFRldSHB_jXVso3jiq8tI8rx1HfSk24mVOuUZkZ7btBCsEjVwFrSRASKivs3k307nEWSY5LY1NAaHfsFG-4nTLlfTcqby4PnMtbFg1ZpoNuk_IJHurzWBQQfd_W4zzGQp6tYwSLuAwOTymDp4L3ItTByid-VjTs6hFdxdO2sg1256K9KSKL3Kg0hx-0D-Uw_JP-KBqXfH-c-vJLduDZXaATZC/p.jpeg" alt="" />
                   </a>
                 </div>
               </li>
               <li>
                 <div className="cardtab">
                   <a href="https://www.parishdental.co.uk/">
-                    <img src="img/portfolio/1.2.jpg" alt="" />
+                    <img src="https://previews.dropbox.com/p/thumb/ACCIswieIwnmlWVjBPXNjmMke64d22GNP5oXvPaEiDzwfeCeH4KiP0QnqdQ52mpOA6ohoi-ksWE4U9eQWVUH1ZSqaHbyTzGNw-2PbTK95BBWO44sU-IqtJ-r3UdOfCDSL99jvK1xpj4TDesyWdstexDu5DIo6ZMGKBkRJRFPA1s-Yro9P2wF9rTOxZs3zzvEXcNHemG58r4Nct3cVguln5CSOcksX2k54DjFbSTxnDh3wVN-IPFOGxL3-kc6xbG1b0nXRf4jGk9NAGiFk8QfwjSUOwh-EbikfG3jZvLfuGF2MQ8ciJ5Ig0HSkgX5B9NeLUpBdswMtp_AqW2MXLL0ZfAc/p.jpeg" alt="" />
                   </a>
                 </div>
               </li>
               <li>
                 <div className="cardtab">
                   <a  >
-                    <img src="img/portfolio/5.2.jpg" alt="" />
+                    <img src="https://previews.dropbox.com/p/thumb/ACCuP35Sdx9mg3c-lEjsgSb_6egp4xi7lCp4uB4eGCLPfOm5B3Ewu_CmHSbz-PuYIbvq5GqZ2zrkiwaAiQzgyt9s8yh62nNFMnRMZeRWWAl8-6vUdxd7kiU4NP5Zs331uBu4hGnfoLXb7TRYfC-PZkt8XgkhjBCdiDFmYKRBrkr9yaNInnFvdWccVa9y1Am32FTRhqPqqSVUjeY8eP3Ei2Nl0CXCjAjqufsqhwv4vCkMOZZoEF6yJoC0i0BdHfH4iVeB1I9PDJqWOJ4kg1ToAAECY_uIeHC8mS88PCHQKd5HJSI9tmuggqp3Ud2TfrZXC-UyaM7tlaYeU7iRa6nnnekaFIHCQCdsvKJsYYaNiIWxsw/p.jpeg" alt="" />
                   </a>
                 </div>
               </li>
               <li>
                 <div className="cardtab">
                   <a href="https://www.novaskinclinicnelson.co.uk/">
-                    <img src="img/portfolio/1.3.jpg" alt="" />
+                    <img src="https://previews.dropbox.com/p/thumb/ACDw6Xb6QUd60DfbZo4Uw-2pRxIlYWUu3vw4mAIk2ruwvOL_ICQvf7FuPEB9jdvstO9HVvjkDNLN952d9rN2tsPfrpKKIaEGNc7BXqq41Jxssxq4ao_ZBWP351iBjLQEpC1DEcoTCmufP4Cc6p9rc5meFOhA3VNgAYFUOVyzVn4EQJJaU6Jcj1War0LTPP6yQtDP3h4r-0CdVolpRREOoPQ_blFboOHDl2JarJkzOanFP-czLq4GGikpw3Ou6tFcZHoktESqZN-NOevajzfBY580m1RAbnQB4gIrMO4PI_AGK1iqIpODf7Oo64wemNnpUZdMF-Qtl5_dDDux-OJCF9wJt-yvdsNE4gTI5nOC_L2ryw/p.jpeg" alt="" />
                   </a>
                 </div>
               </li>
               <li>
                 <div className="cardtab">
                   <a href="https://emergency247dentist.co.uk/">
-                    <img src="img/portfolio/1.4.jpg" alt="" />
+                    <img src="https://previews.dropbox.com/p/thumb/ACCgmtcBMkWkL8ohMaBOJsnu1TFBRfrpjUCitKxRbHwnXoVH2e8H3wSCDDIhYlipq0TSfCOxCBR-nfsXzrarcvPqYdNFkH_TUoDu62hp69Y1i5QCb8yrJgXIiMlNVwgMAWIST3KSaUdIRFjQ8k4QSCDBwnDB8pO1GqCWC4-rOuQiJeWEQEVhjx_9kZlHdDaK54Hle7xOTaQuXkpTZLjqwinuJXdChX-5O3CfQiS8iA34O0Mikg8dZHFrggoaROda2-ia9eFUEHQTYQbYyA6Tgk6pBcJD77yGyGKTo7XTUFIZ-q-y1EEcdjr0FtlG4-P0j465BmNVPiA3JZMPt9A9BD_qU0MRDF8Za15css92ZpM-vg/p.jpeg" alt="" />
                   </a>
                 </div>
               </li>
@@ -129,35 +129,35 @@ const Portfolo = () => {
               <li>
                 <div className="cardtab">
                   <a href="https://marspurifier.com/" >
-                    <img src="img/portfolio/1.1.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/db1i46uiv/image/upload/v1693074698/1.1_hffh2b.jpg" alt="" />
                   </a>
                 </div>
               </li>
               <li>
                 <div className="cardtab">
                 <a href="https://www.parishdental.co.uk/">
-                    <img src="img/portfolio/1.2.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/db1i46uiv/image/upload/v1693074702/1.2_uszdxe.jpg" alt="" />
                   </a>
                 </div>
               </li>
               <li>
                 <div className="cardtab">
                 <a href="https://www.novaskinclinicnelson.co.uk/">
-                    <img src="img/portfolio/1.3.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/db1i46uiv/image/upload/v1693074708/1.3_gofnpz.jpg" alt="" />
                   </a>
                 </div>
               </li>
               <li>
                 <div className="cardtab">
                 <a href="https://emergency247dentist.co.uk/">
-                    <img src="img/portfolio/1.4.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/db1i46uiv/image/upload/v1693074703/1.4_kyeuoa.jpg" alt="" />
                   </a>
                 </div>
               </li>
               <li>
                 <div className="cardtab">
                 <a href="https://cdtrainingacademy.co.uk/">
-                    <img src="img/portfolio/1.5.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/db1i46uiv/image/upload/v1693074706/1.5_czn1rv.jpg" alt="" />
                   </a>
                 </div>
               </li>
